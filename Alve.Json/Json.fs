4c4c48e965ac4486117eac3da4db60c5d1f34608
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-06 13:20:54 +0200

    Add decoders for json primitives

--- Alve.Json/Json.fs
+++ Alve.Json/Json.fs
@@ -1,2 +1,47 @@
 ﻿module Json
 
+open System
+open System.Text.Json
+
+module Decode =
+    type Decoder<'a> = JsonElement -> Result<'a, string>
+
+    let private expectationFailed (expected: string) (got: 'a) = Error (sprintf "Expected: %s, but got: %O" expected got)
+
+    let jstring: Decoder<string> = fun json ->
+        match json.ValueKind with
+            | JsonValueKind.String -> Ok (json.GetString ())
+            | other -> expectationFailed "String" other
+
+    let jbool: Decoder<bool> = fun json ->
+        match json.ValueKind with
+            | JsonValueKind.True -> Ok true
+            | JsonValueKind.False -> Ok false
+            | other -> expectationFailed "Boolean" other
+
+    let jint: Decoder<int64> = fun json ->
+        match json.ValueKind with
+            | JsonValueKind.Number -> 
+                try
+                    Ok (json.GetInt64())
+                with
+                    | :? FormatException as e -> Error e.Message
+            | other -> expectationFailed "Int" other
+
+    let jfloat: Decoder<float> = fun json ->
+        match json.ValueKind with
+            | JsonValueKind.Number ->
+                try
+                    Ok (json.GetDouble())
+                with
+                    | :? FormatException as e -> Error e.Message
+            | other -> expectationFailed "Float" other
+
+    let jdecimal: Decoder<decimal> = fun json ->
+        match json.ValueKind with
+            | JsonValueKind.Number ->
+                Ok (json.GetDecimal())
+            | other -> expectationFailed "Decimal" other
+
+module Encode = 
+    ()
