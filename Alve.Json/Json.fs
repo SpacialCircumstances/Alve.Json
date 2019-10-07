574394e4409d9be55e1d30e490f1530d051d17e2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-07 21:22:36 +0200

    Create ok computation expression to make chain decoding easier without map

--- Alve.Json/Json.fs
+++ Alve.Json/Json.fs
@@ -204,5 +204,15 @@ module Decode =
 
             | other -> expectationFailed "Array" other
 
+    type SuccessBuilder() =
+        member this.Bind(x, f) = 
+            match x with
+                | Error e -> Error e
+                | Ok a -> f a
+        
+        member this.Return(x) = Ok x
+        
+    let ok = SuccessBuilder()
+
 module Encode = 
     ()
